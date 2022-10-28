2102e6ada7d6818a3d1762d618f33f8b9a661a00
versorspace <resoaxes@gmail.com>
2022-10-28 13:29:01 +0530

    remove null character

--- termwordclip.sh
+++ termwordclip.sh
@@ -1,6 +1,6 @@
 #!/run/current-system/sw/bin/bash
 
-tee /tmp/termout | awk -v OFS="\n" '{$1=$1}1' | cat - /tmp/termout | sed -r 's/^[ \t]*//g' | sort -r | uniq | dmenu -i | tr -d '\n' | xclip -i
+tr -d '\0' | tee /tmp/termout | awk -v OFS="\n" '{$1=$1}1' | cat - /tmp/termout | sed -r 's/^[ \t]*//g' | sed -r 's/[ \t]*$//g' | sort -r | uniq | dmenu -i | tr -d '\n' | xclip -i
 
 # sed '/^$/d' | sed '/^ *$/d' | tee /tmp/termout | tr ' ' '\n' | sed '/^ *$/d' | sed '/^$/d' >> /tmp/termout
 # sed -e 's/^[ \t]*//g' /tmp/termout | dmenu | tr -d '\n' | xclip -i
